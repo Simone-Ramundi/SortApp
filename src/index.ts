430fdec3578fce55a85a793efabb0b8bab1dad97
Simone Ramundi <simone.ramundi@4science.com>
2023-11-22 11:59:42 +0100

    sorting implementation

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,16 @@
 class Sorter {
     constructor(public collection: number[]) {}
     sort():void {
-
+    const { length } = this.collection;
+        for (let i = 0; i < length; i++) {
+          for (let j = 0; j < length - i - 1; j++) {
+              if(this.collection[j] > this.collection[j + 1]) {
+                  const leftHand = this.collection[j];
+                  this.collection[j] = this.collection[j+1];
+                  this.collection[j +1] = leftHand;
+              }
+          }
+        }
     }
 }
 
