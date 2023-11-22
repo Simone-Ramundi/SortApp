bdfadce1b5a04dccabfea78951c6680e61d3ea7e
Simone Ramundi <simone.ramundi@4science.com>
2023-11-22 16:36:33 +0100

    updated for linked list

--- build/index.js
+++ build/index.js
@@ -1,14 +1,25 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const Sorter_1 = require("./Sorter");
-const CharactersCollection_1 = require("./CharactersCollection");
+const LinkedList_1 = require("./LinkedList");
 /* SORTING NUMBERS */
 /*const numberCollection = new NumbersCollection([10, 3, -5, 0]);
 const sorter = new Sorter(numberCollection);
 sorter.sort();
 console.log(numberCollection.data);*/
 /* SORTING LETTERS */
-const charactersCollection = new CharactersCollection_1.CharactersCollection('Xaayb');
-const sorter = new Sorter_1.Sorter(charactersCollection);
+/*
+const charactersCollection = new CharactersCollection('Xaayb');
+const sorter = new Sorter(charactersCollection);
 sorter.sort();
-console.log(charactersCollection.data);
+console.log(charactersCollection.data)
+*/
+/* SORTING LINKED LIST */
+const linkedList = new LinkedList_1.LinkedList();
+linkedList.add(500);
+linkedList.add(-10);
+linkedList.add(-3);
+linkedList.add(4);
+const sorter = new Sorter_1.Sorter(linkedList);
+sorter.sort();
+linkedList.print();
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import {Sorter} from "./Sorter";
 import {NumbersCollection} from "./NumbersCollection";
 import { CharactersCollection } from "./CharactersCollection";
+import {LinkedList} from "./LinkedList";
 
 
 /* SORTING NUMBERS */
@@ -10,8 +11,18 @@ sorter.sort();
 console.log(numberCollection.data);*/
 
 /* SORTING LETTERS */
+/*
 const charactersCollection = new CharactersCollection('Xaayb');
 const sorter = new Sorter(charactersCollection);
 sorter.sort();
 console.log(charactersCollection.data)
-
+*/
+/* SORTING LINKED LIST */
+    const linkedList = new LinkedList();
+    linkedList.add(500);
+    linkedList.add(-10);
+    linkedList.add(-3);
+    linkedList.add(4);
+    const sorter = new Sorter(linkedList);
+    sorter.sort();
+    linkedList.print();
