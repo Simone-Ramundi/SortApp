152f1879539a1f6b9f65d044c82108439eb5d719
Simone Ramundi <simone.ramundi@4science.com>
2023-11-22 13:08:03 +0100

    correct soluution

--- build/index.js
+++ build/index.js
@@ -1,30 +1,8 @@
 "use strict";
-class Sorter {
-    constructor(collection) {
-        this.collection = collection;
-    }
-    sort() {
-        const { length } = this.collection;
-        for (let i = 0; i < length; i++) {
-            for (let j = 0; j < length - i - 1; j++) {
-                // All of this only work if collection is number[]
-                // If collection is an array of numbers
-                if (this.collection instanceof Array) {
-                    if (this.collection[j] > this.collection[j + 1]) {
-                        const leftHand = this.collection[j];
-                        this.collection[j] = this.collection[j + 1];
-                        this.collection[j + 1] = leftHand;
-                    }
-                }
-                // Only going to work if collection is a string
-                // If collection is a string, do this logic instead:
-                // --logic to compare and swap characters in a string
-                if (typeof this.collection === "string") {
-                }
-            }
-        }
-    }
-}
-const sorter = new Sorter([10, 3, -5, 0]);
+Object.defineProperty(exports, "__esModule", { value: true });
+const Sorter_1 = require("./Sorter");
+const NumbersCollection_1 = require("./NumbersCollection");
+const numberCollection = new NumbersCollection_1.NumbersCollection([10, 3, -5, 0]);
+const sorter = new Sorter_1.Sorter(numberCollection);
 sorter.sort();
-console.log(sorter.collection);
+console.log(numberCollection.data);
