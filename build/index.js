3097784dc47c067cae5dcefa64fcd02da7f8c6ca
Simone Ramundi <simone.ramundi@4science.com>
2023-11-22 15:48:20 +0100

    Sorting Arbitrary Collections

--- build/index.js
+++ build/index.js
@@ -1,8 +1,14 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const Sorter_1 = require("./Sorter");
-const NumbersCollection_1 = require("./NumbersCollection");
-const numberCollection = new NumbersCollection_1.NumbersCollection([10, 3, -5, 0]);
-const sorter = new Sorter_1.Sorter(numberCollection);
+const CharactersCollection_1 = require("./CharactersCollection");
+/* SORTING NUMBERS */
+/*const numberCollection = new NumbersCollection([10, 3, -5, 0]);
+const sorter = new Sorter(numberCollection);
 sorter.sort();
-console.log(numberCollection.data);
+console.log(numberCollection.data);*/
+/* SORTING LETTERS */
+const charactersCollection = new CharactersCollection_1.CharactersCollection('Xaayb');
+const sorter = new Sorter_1.Sorter(charactersCollection);
+sorter.sort();
+console.log(charactersCollection.data);
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,17 @@
 import {Sorter} from "./Sorter";
 import {NumbersCollection} from "./NumbersCollection";
+import { CharactersCollection } from "./CharactersCollection";
 
-const numberCollection = new NumbersCollection([10, 3, -5, 0]);
+
+/* SORTING NUMBERS */
+/*const numberCollection = new NumbersCollection([10, 3, -5, 0]);
 const sorter = new Sorter(numberCollection);
 sorter.sort();
-console.log(numberCollection.data);
+console.log(numberCollection.data);*/
+
+/* SORTING LETTERS */
+const charactersCollection = new CharactersCollection('Xaayb');
+const sorter = new Sorter(charactersCollection);
+sorter.sort();
+console.log(charactersCollection.data)
+
