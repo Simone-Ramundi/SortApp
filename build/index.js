7f498e12626826e6bc5797830d30b66357685030
Simone Ramundi <simone.ramundi@4science.com>
2023-11-22 17:12:09 +0100

    turning in abstract class

--- build/index.js
+++ build/index.js
@@ -1,11 +1,14 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-const Sorter_1 = require("./Sorter");
-const LinkedList_1 = require("./LinkedList");
 /* SORTING NUMBERS */
-/*const numberCollection = new NumbersCollection([10, 3, -5, 0]);
+/*
+const numberCollection = new NumbersCollection([10, 3, -5, 0]);
 const sorter = new Sorter(numberCollection);
 sorter.sort();
+console.log(numberCollection.data);
+*/
+/*const numberCollection = new NumbersCollection([10, 3, -5, 0]);
+numberCollection.sort();
 console.log(numberCollection.data);*/
 /* SORTING LETTERS */
 /*
@@ -14,12 +17,15 @@ const sorter = new Sorter(charactersCollection);
 sorter.sort();
 console.log(charactersCollection.data)
 */
+/*const charactersCollection = new CharactersCollection('Xaayb');
+charactersCollection.sort();
+console.log(charactersCollection.data);*/
 /* SORTING LINKED LIST */
-const linkedList = new LinkedList_1.LinkedList();
-linkedList.add(500);
-linkedList.add(-10);
-linkedList.add(-3);
-linkedList.add(4);
-const sorter = new Sorter_1.Sorter(linkedList);
-sorter.sort();
-linkedList.print();
+/*    const linkedList = new LinkedList();
+    linkedList.add(500);
+    linkedList.add(-10);
+    linkedList.add(-3);
+    linkedList.add(4);
+
+    linkedList.sort();
+    linkedList.print();*/
--- src/index.ts
+++ src/index.ts
@@ -5,9 +5,15 @@ import {LinkedList} from "./LinkedList";
 
 
 /* SORTING NUMBERS */
-/*const numberCollection = new NumbersCollection([10, 3, -5, 0]);
+/*
+const numberCollection = new NumbersCollection([10, 3, -5, 0]);
 const sorter = new Sorter(numberCollection);
 sorter.sort();
+console.log(numberCollection.data);
+*/
+
+/*const numberCollection = new NumbersCollection([10, 3, -5, 0]);
+numberCollection.sort();
 console.log(numberCollection.data);*/
 
 /* SORTING LETTERS */
@@ -17,12 +23,17 @@ const sorter = new Sorter(charactersCollection);
 sorter.sort();
 console.log(charactersCollection.data)
 */
+
+/*const charactersCollection = new CharactersCollection('Xaayb');
+charactersCollection.sort();
+console.log(charactersCollection.data);*/
+
 /* SORTING LINKED LIST */
-    const linkedList = new LinkedList();
+/*    const linkedList = new LinkedList();
     linkedList.add(500);
     linkedList.add(-10);
     linkedList.add(-3);
     linkedList.add(4);
-    const sorter = new Sorter(linkedList);
-    sorter.sort();
-    linkedList.print();
+
+    linkedList.sort();
+    linkedList.print();*/
