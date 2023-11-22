e10a59b704c07c6794b706f21a8e1a3faf3ed1de
Simone Ramundi <simone.ramundi@4science.com>
2023-11-22 12:39:09 +0100

    bad code

--- build/index.js
+++ build/index.js
@@ -7,10 +7,19 @@ class Sorter {
         const { length } = this.collection;
         for (let i = 0; i < length; i++) {
             for (let j = 0; j < length - i - 1; j++) {
-                if (this.collection[j] > this.collection[j + 1]) {
-                    const leftHand = this.collection[j];
-                    this.collection[j] = this.collection[j + 1];
-                    this.collection[j + 1] = leftHand;
+                // All of this only work if collection is number[]
+                // If collection is an array of numbers
+                if (this.collection instanceof Array) {
+                    if (this.collection[j] > this.collection[j + 1]) {
+                        const leftHand = this.collection[j];
+                        this.collection[j] = this.collection[j + 1];
+                        this.collection[j + 1] = leftHand;
+                    }
+                }
+                // Only going to work if collection is a string
+                // If collection is a string, do this logic instead:
+                // --logic to compare and swap characters in a string
+                if (typeof this.collection === "string") {
                 }
             }
         }
